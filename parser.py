32d43b554611dddef08a5101cbf1ce6d38df1849
maxizorin <ganse-0@yandex.ru>
2021-11-06 15:05:14 +0300

    Вывел весь алгоритм в функцию, парсит весь товар, если цены нет выходит из цикла

--- parser.py
+++ parser.py
@@ -3,28 +3,55 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-if __name__ == '__main__':
+
+def format_price(price):
+    pre_price = str(price)
+    pre_price = pre_price[0:-1]
+    return int(pre_price.replace(' ', ''))
+
+
+def scraping(url, driver):
+    global price_promo, price
     opts = Options()
     opts.headless = True
-    chrome = Chrome('chromedriver', chrome_options=opts)
-    page = 25
+    chrome = Chrome(driver, chrome_options=opts)
+    page = 21
+    index = 1
+    items_list = []
     while True:
-        url = 'https://www.detmir.ru/catalog/index/name/lego/page/{}/'.format(page)
-        chrome.get(url)
+        url_with_page = url + str(page)
+        chrome.get(url_with_page)
         items = chrome.find_elements(By.XPATH, '//a[contains(@href,"https://www.detmir.ru/product/index/id/")]')
-        index = 1
+
         for item in items:
-            print('Item {}'.format(index,))
-            p_elem = list(item.find_elements(By.TAG_NAME,'p'))
-            print('Prices', len(p_elem))
-            for i in p_elem:
-                print(i.text)
-            index += 1
-            # break
-        break
+            print('Item {}'.format(index, ))
+            p_elem = list(item.find_elements(By.TAG_NAME, 'p'))
+            if len(p_elem) > 1:
+                id_item = item.get_attribute('href').split('/')[6]
+                url_item = item.get_attribute('href')
+                title = p_elem[0].text
+                if len(p_elem) == 3:
+                    # print(p_elem[1].text, p_elem[2].text)
+                    price_promo = format_price(p_elem[1].text)
+                    price = format_price(p_elem[2].text)
+                    index += 1
+                elif len(p_elem) == 2:
+                    price = format_price(p_elem[1].text)
+                    price_promo = False
+                provisional_dict = {
+                    'id': id_item,
+                    'title': title,
+                    'price': price,
+                    'promo_price': price_promo,
+                    'url': url_item
+                }
+                print(provisional_dict)
+                items_list.append(provisional_dict)
+            else:
+                return
+        page += 1
 
-
-
-
-
-
+if __name__ == '__main__':
+    driver = 'chromedriver'
+    url = 'https://www.detmir.ru/catalog/index/name/lego/page/'
+    scraping(url, driver)
